dae1c6f611263d48c98d180e9870f025a4c3ee3a
malickmoro <150017548+malickmoro@users.noreply.github.com>
2023-12-10 10:35:05 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,5 +2,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyCam"
+    name: "SwiftyCam",
+    products: [
+        // Define one or more products here.
+        .library(
+            name: "SwiftyCam",
+            targets: ["SwiftyCam"]
+        ),
+    ],
+    dependencies: [
+        // List your package's dependencies here, if any.
+    ],
+    targets: [
+        // Define one or more targets here.
+        .target(
+            name: "SwiftyCam",
+            dependencies: [],
+            path: "Source"  // Specify the path to the source files.
+        ),
+    ]
 )
